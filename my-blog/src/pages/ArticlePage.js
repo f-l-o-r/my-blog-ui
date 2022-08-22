d9542e8db49717d2b7bfdc3f5b4fef209f471430
Flor <flor.vi93@gmail.com>
2022-08-22 15:41:14 +0900

    updating the blog

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+// import articlesContent from '../data/Articles-content';
 
 const ActiclePage = () => {
-    let { name } = useParams();
-    return (
-        <>
-            <h1>This is an {name}  article</h1>
-        </>
-    )
+    // let { name } = useParams();
+    // return (
+    //     <>
+    //         <h1>This is an {name}  article</h1>
+    //     </>
+    // )
 }
 
 export default ActiclePage;
--- my-blog/src/pages/Home.js
+++ my-blog/src/pages/Home.js
@@ -2,10 +2,28 @@ import React from 'react';
 
 const HomePage = () => (
     <>
-        <h1>Welcome to my Blog</h1>
-        <p>
-            This blog will be part of practice
-        </p>
+        <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0">
+            
+        <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src={require('../img/wp.jpeg')} alt="" width="384" height="512"/>
+        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+            <blockquote>
+            <div className="text-lg font-medium whitespace-pre-line w-3/4 ">
+                Hi!
+                I'm a software developer that I'm mainly focused in the back end side using spring boot as main framework following the micro-services concept, 
+                but also I have experience working in the front end side using JavaScript libraries and framework.
+
+            </div>
+            </blockquote>
+            <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text-sky-400">
+                Flor Ortiz
+            </div>
+            <div className="text-slate-700 dark:text-slate-500">
+                Full stack developer
+            </div>
+            </figcaption>
+        </div>
+        </figure>
     </>
 );
 
